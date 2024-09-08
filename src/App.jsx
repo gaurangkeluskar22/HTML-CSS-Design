1507cdfa4600ecab7af8677e1850eb96779f058f
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2024-09-08 20:53:00 +0530

    removed lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,23 @@
 import "./App.css";
 import { ThemeProvider } from "./context/ThemeContext";
 import { lazy, Suspense } from "react";
-const NavBar = lazy(() => import("./Component/NavBar/NavBar"));
-const About = lazy(() => import("./Component/About/About"));
-const Skills = lazy(() => import("./Component/Skills/Skills"));
-
+// const NavBar = lazy(() => import("./Component/NavBar/NavBar"));
+// const About = lazy(() => import("./Component/About/About"));
+// const Skills = lazy(() => import("./Component/Skills/Skills"));
+import NavBar from './Component/NavBar/NavBar'
+import About from './Component/About/About'
+import Skills from './Component/Skills/Skills'
 
 function App() {
   return (
     <ThemeProvider>
       <div className="wrapper">
         <div className="navbar-outer-wrapper">
-          <Suspense fallback={<div></div>}>
             <NavBar />
-          </Suspense>
         </div>
         <div className="body-wrapper">
-          <Suspense fallback={<div></div>}>
             <About />
-          </Suspense>
-          <Suspense fallback={<div></div>}>
             {/* <Skills /> */}
-          </Suspense>
         </div>
       </div>
     </ThemeProvider>
